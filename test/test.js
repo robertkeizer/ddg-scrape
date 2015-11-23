d0c8eb752fe41c81720f7674f777c2dcbc7cfa66
Robert Keizer <robert@keizer.ca>
2015-11-22 22:43:24 -0600

    Working spooky..

--- test/test.js
+++ test/test.js
@@ -2,7 +2,38 @@ var assert = require( "assert" );
 
 describe( "Duck Duck Go Scrape", function( ){
 	
-	it( "Is a module", function( ){
-		
+	it( "Is a function", function( ){
+		var ddgs = require( "../lib/index.js" );
+		assert.equal( "function", typeof( ddgs ) );
+	} );
+
+	it( "Returns an instance", function( ){
+		var ddgs = require( "../lib/index.js" );
+		var _ddgs = new ddgs( );
+		assert.ok( _ddgs instanceof ddgs );
+	} );
+
+	describe( "Instance", function( ){
+		var _instance;
+		before( function( ){
+			var ddgs = require( "../lib/index.js" );
+			_instance = new ddgs( );
+		} );
+
+		it( "Has Search function", function( ){
+			assert.equal( "function", typeof( _instance.Search ) );
+		} );
+
+		it( "Search function must have a callback", function( ){
+			assert.throws( function( ){
+				_instance.Search( "Something to search" );
+			} );
+		} );
+
+		it( "Returns results", function( cb ){
+			_instance.Search( "Walmart", function( err, results ){
+				return cb( err, results );
+			} );
+		} );
 	} );
 } );
